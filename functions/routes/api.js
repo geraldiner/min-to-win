c7c83664ba2660dcb94494d404ed79f79f6382d8
Geraldine <hello@geraldiner.com>
2021-11-11 21:52:13 -0500

    rebundle

--- functions/routes/api.js
+++ functions/routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const apiController = require("../controller/api");
 
+router.get("/", apiController.getIndex);
 router.get("/games", apiController.getGames);
 router.get("/games/:gameId", apiController.getGame);
 router.post("/games", apiController.postGames);
